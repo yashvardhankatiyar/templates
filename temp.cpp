9db75530424ebcfc60def1fb85fa5dfe9913da44
yashvardhankatiyar <ykatiyar0902@gmail.com>
2024-03-12 12:02:57 +0530

    first commit

---
+++ temp.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define pp pop_back
+using ll = long long;
+
+#define fr(n) for(ll i=0; i<n; i++)
+#define vi vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+
+#define ff first 
+#define ss second
+#define PI 3.141592653589793238462
+
+using ull = unsigned long long;
+using lld = long double;
+
+
+
